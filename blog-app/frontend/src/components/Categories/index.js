cd6cac8efc880895cd1c71b5c11b92882f73f394
Caleb Kwakye <ckwakyeapps7@gmail.com>
2024-07-14 00:57:34 +0200

    updated categories page and categories list per homework

--- blog-app/frontend/src/components/Categories/index.js
+++ blog-app/frontend/src/components/Categories/index.js
@@ -1,24 +1,59 @@
+
+// Third party
 import React from "react";
-import "./index.css"
 
+// Components
+import Navbar from "../../components/Navbar";
+import Heading from "../../components/Heading";
+import CategoryList from "../../components/CategoryList";
+import Footer from "../../components/Footer";
 
-export default function Categories({ blogPost }) {
+// Styles
+import "../../App.css";
+
+// Week 1: Import the blogPosts and categories from the dummy-data.json file
+const data = require("../../dummy-data.json");
+const categories = data.categories;
+
+export default function Categories() {
+
   return (
-    <div className="flex-wrap">
-      {blogPost.categories.map((category, index) => {
-        return (
-          <p
-            key={index}
-            className="category-tag"
-            style={{
-              color: category.color,
-              backgroundColor: category.color + "33",
-            }}
-          >
-            {category.title}
-          </p>
-        );
-      })}
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Heading />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <p className="page-subtitle">Categories</p>
+        </div>
+        <CategoryList categories={categories}></CategoryList>
+      </div>
+      <Footer />
+    </>
   );
 }
+
+
+// import React from "react";
+// import "./index.css"
+
+
+// export default function Categories({ blogPost }) {
+//   return (
+//     <div className="flex-wrap">
+//       {blogPost.categories.map((category, index) => {
+//         return (
+//           <p
+//             key={index}
+//             className="category-tag"
+//             style={{
+//               color: category.color,
+//               backgroundColor: category.color + "33",
+//             }}
+//           >
+//             {category.title}
+//           </p>
+//         );
+//       })}
+//     </div>
+//   );
+// }
